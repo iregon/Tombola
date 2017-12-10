73e4ed53c4d04bb04d676d9229e1aa8c0fda9760
iregon <0iregon0@gmail.com>
2017-12-10 11:24:12 +0100

    base pagina cartella

--- www/templates/cartella/js/cartellaController.js
+++ www/templates/cartella/js/cartellaController.js
@@ -1,4 +1,93 @@
 angular.module('starter.controllers')
-.controller('CartellaCtrl', function($scope) {
-  $scope.cartelle = ["Saab", "Volvo", "BMW"];
+.controller('CartellaCtrl', function($scope, $stateParams) {
+
+  var cartella = {
+    numero: 1,
+    righe: {
+      0: {
+        0: "",
+        1: "16",
+        2: "21",
+        3: "31",
+        4: "42",
+        5: "",
+        6: "6",
+        7: "",
+        8: "",
+      },
+      1: {
+        0: "1",
+        1: "18",
+        2: "",
+        3: "33",
+        4: "",
+        5: "53",
+        6: "62",
+        7: "",
+        8: "",
+      },
+      2: {
+        0: "2",
+        1: "",
+        2: "",
+        3: "38",
+        4: "",
+        5: "55",
+        6: "69",
+        7: "74",
+        8: "",
+      }
+    }
+  };
+
+  var cartella2 = {
+    numero: 2,
+    righe: {
+      0: {
+        0: "",
+        1: "16",
+        2: "21",
+        3: "31",
+        4: "42",
+        5: "",
+        6: "6",
+        7: "",
+        8: "",
+      },
+      1: {
+        0: "1",
+        1: "18",
+        2: "",
+        3: "33",
+        4: "",
+        5: "53",
+        6: "62",
+        7: "",
+        8: "",
+      },
+      2: {
+        0: "2",
+        1: "",
+        2: "",
+        3: "38",
+        4: "",
+        5: "55",
+        6: "69",
+        7: "74",
+        8: "",
+      }
+    }
+  };
+
+  var cartelle = [cartella, cartella2];
+
+  $scope.getCartella = function() {
+    var numCartella = $stateParams.num;
+    // console.log(JSON.stringify(cartelle[numCartella]));
+    return cartelle[numCartella];
+  }
+
+  $scope.getAllCartelle = function() {
+    return cartelle;
+  }
 });
