690c7af28c023a04d1b6a0b53800f813a2e134fc
iregon <0iregon0@gmail.com>
2017-12-10 15:13:01 +0100

    pagina login

--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,32 @@
 angular.module('starter')
 .service("serverManager", function() {
+  var socket;
   return {
     connectToServer: function() {
       // Open a WebSocket connection
-      var socket = new WebSocket('ws://127.0.0.1:8080/Tombola_Server_NetBeans/actions');
+      socket = new WebSocket('ws://127.0.0.1:8080/Tombola_Server_NetBeans/actions');
       console.log("connessione");
+    },
+    sendData: function(data) {
+      this.send = function (message) {
+        this.waitForConnection(function () {
+          socket.send(message);
+        }, 1000);
+      };
+
+      this.waitForConnection = function (callback, interval) {
+        if (socket.readyState === 1) {
+          callback();
+        } else {
+          var that = this;
+          // optional: implement backoff for interval here
+          setTimeout(function () {
+            that.waitForConnection(callback, interval);
+          }, interval);
+        }
+      };
+
+      this.send(JSON.stringify(data));
     }
   }
 });
--- www/templates/login/js/loginController.js
+++ www/templates/login/js/loginController.js
@@ -1,4 +1,21 @@
 angular.module('starter')
-.controller('LoginCtrl', function($scope, serverManager) {
+.controller('LoginCtrl', function($scope, $rootScope, serverManager) {
   serverManager.connectToServer();
+
+  $scope.doLogin = function() {
+    var data = {
+      nome: $scope.nickname,
+      anunymous: "false",
+    }
+    $rootScope.userData = data;
+  }
+
+  $scope.doLoginAnonymous = function() {
+    var data = {
+      nome: "",
+      anunymous: "true",
+    }
+    $rootScope.userData = data;
+  }
+
 });
